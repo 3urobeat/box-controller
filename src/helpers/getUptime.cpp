801df5cb47608c83865fcbe1cace0b0235aeeaef
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2022-09-08 18:04:03 +0200

    Fix hour showing 1 second to late

--- src/helpers/getUptime.cpp
+++ src/helpers/getUptime.cpp
@@ -4,7 +4,7 @@
  * Created Date: 07.09.2022 14:42:31
  * Author: 3urobeat
  * 
- * Last Modified: 08.09.2022 13:38:48
+ * Last Modified: 08.09.2022 18:02:46
  * Modified By: 3urobeat
  * 
  * Copyright (c) 2022 3urobeat <https://github.com/HerrEurobeat>
@@ -31,7 +31,7 @@ char* getUptime(char *dest) {
     dest[0] = '\0'; // make sure dest is empty (can't use memset here as we can't use sizeof(), so we only set first byte to 0 which is enough for strcat to work correctly)
 
     // Add hours and divider if uptime >1 hour
-    if (uptime > 3600) {
+    if (uptime >= 3600) {
         uint8_t hours = uptime / 3600;
         lcd.toFixedLengthNumber(numBuf, hours, 2);
         strcat(dest, numBuf);
